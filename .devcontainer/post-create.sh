13c75e6206733ed2706cecd8a3ecdd2b1cce3281
Norihiro Seto <norihiros@acm.org>
2025-04-27 15:28:18 +0900

    Add devcontainer config

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Execute commands after container creation
+echo "Starting setup after container creation"
+
+# Check Go version
+go version
+
+# Install necessary tools
+# Example: go get commands or other setup tasks
+
+echo "Setup completed"
